9124648103eea07579c024a5b314591eb5084f75
Sean Patrick Hagen <sean.hagen@gmail.com>
2016-01-14 00:16:28 -0800

    Remove test functions

--- server.go
+++ server.go
@@ -2,45 +2,20 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"github.com/gchaincl/dotsql"
 	"github.com/julienschmidt/httprouter"
-	"io"
 	"log"
 	"net/http"
 )
 
-func test(db *sql.DB, dot *dotsql.DotSql) httprouter.Handle {
-	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		loc, err := loadLocation(db, dot, "5036 Hasting")
-
-		if err != nil {
-			fmt.Printf("Error loading location: %#v\n", err)
-			panic(err)
-		}
-
-		io.WriteString(w, loc.Name)
-		fmt.Printf("%#v\n", loc)
-	}
-}
-
-func testParam(db *sql.DB, dot *dotsql.DotSql) httprouter.Handle {
-	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		loc, err := loadLocation(db, dot, ps.ByName("address"))
-
-		if err != nil {
-			fmt.Printf("Error loading location: %#v\n", err)
-			panic(err)
-		}
-
-		io.WriteString(w, loc.Name)
-	}
-}
-
 func server(db *sql.DB, dot *dotsql.DotSql) {
 	router := httprouter.New()
-	router.GET("/", test(db, dot))
-	router.GET("/test/:address", testParam(db, dot))
+
+	//router.GET("/locations", listLocations(db, dot))
+	//router.GET("/locations/:id", fetchLocation(db, dot))
+	router.GET("/locations/search/:address", searchLocation(db, dot))
+
+	//router.POST("/locations", createLocation(db, dot))
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
