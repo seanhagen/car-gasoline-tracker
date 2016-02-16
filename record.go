88fc79a9ec4fb6e642dcddf98793d25646ba5c93
Sean Patrick Hagen <sean.hagen@gmail.com>
2016-02-15 23:55:42 -0800

    Remove some fields from struct
    Don't think I really need these.

--- record.go
+++ record.go
@@ -6,13 +6,12 @@ import (
 )
 
 type Record struct {
-	UUID       string
-	LocationId string  // location of the gas station
-	Odometer   uint32  // odometer reading when gas purchased
-	Liters     float32 // amount of gas purchased
-	Cost       uint16  // cost in cents
-	_saved     bool
-	_changed   bool
+	UUID         string
+	LocationUUID string  // location of the gas station
+	Odometer     uint32  // odometer reading when gas purchased
+	Liters       float32 // amount of gas purchased
+	Cost         uint16  // cost in cents
+}
 }
 
 func (r *Record) create(r *http.Request) error {
